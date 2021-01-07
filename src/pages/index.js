ecfc49155b498baa9ed4e0964caad61b78a3832f
Antoniel Magalhães <antoniel2210@gmail.com>
2021-01-06 21:27:37 -0300

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Movie List</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>Movie List</div>
+    </>
+  );
+}
