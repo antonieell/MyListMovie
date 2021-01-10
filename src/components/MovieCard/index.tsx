a11db1b81993cb325786c29b793496c3ef3d9b7d
Antoniel Magalhães <antoniel2210@gmail.com>
2021-01-10 13:55:51 -0300

    feat: improve image perfomance

--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -1,4 +1,5 @@
 import { Result } from "src/types/tmdb";
+import Image from "next/image";
 
 interface MovieCard {
   value: Result;
@@ -7,10 +8,17 @@ interface MovieCard {
 const MovieCard = ({ value }: MovieCard) => {
   return (
     <div
-      className="w-full mx-auto bg-blue-900 radius-sm hover:bg-gray-800"
+      className="w-full mx-auto bg-blue-900 radius-sm rounded-xl hover:opacity-95 hover:bg-gray-800"
       style={{ maxWidth: "500px" }}
     >
-      <img src={`https://image.tmdb.org/t/p/w500/${value.poster_path}`} />
+      <Image
+        width={342}
+        height={513}
+        layout="responsive"
+        className="object-cover object-center w-full h-full rounded-xl"
+        src={`https://image.tmdb.org/t/p/w342/${value.poster_path}`}
+        alt={`${value.original_title}`}
+      />
       <div className="p-2 ">
         <p className="overflow-hidden text-center overflow-ellipsis whitespace-nowrap">
           {value.original_title}
